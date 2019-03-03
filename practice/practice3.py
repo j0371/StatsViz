a0b176a319681fb05e7b463f6ed249435fab2cf9
j0371 <43154079+j0371@users.noreply.github.com>
2019-03-03 15:27:31 -0600

    test commit

---
+++ practice/practice3.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as mplot
+
+mplot.scatter([5],[5], color="blue")
+
+mplot.plot([3,7,5,5,3,7],[3,3,3,7,7,7], color="blue")
+
+mplot.scatter([20],[20], color="blue")
+
+mplot.plot([18,22,20,20,18,22],[18,18,18,22,22,22], color="blue")
+
+mplot.show()
